e6c269b4381caf1ebb6c7d2a8c8aaf8e56174041
Enol <enol826@hotmail.com>
2018-02-24 23:57:19 +0100

    Add files via upload

---
+++ examples/NumberSum.java
@@ -0,0 +1,61 @@
+package examples;
+
+import source.Algorithm;
+import source.Config;
+
+public class NumberSum extends Algorithm {
+    /*
+        2x^2 + 7 = 0
+        EXAMPLE.
+        PROBLEM
+        Given:
+            1. array of N numbers (e.g. {2,5,9,10})
+            2. array of M possible operations (e.g. {+,-,*})
+            3. a expected result (e.g. 53)
+            How can we combine the numbers and operations given in order to get the expected result
+            We look for:
+                2 [op1] 5 [op2] 9 [op3] 10 = 53
+     */
+
+    /* -------------- FITNESS FUNCTION -------------- */
+    private int operation(int n1, int n2, String op) {
+        switch(op) {
+            case "+":
+                return n1+n2;
+            case "-":
+                return n1-n2;
+            case "*":
+                return n1*n2;
+            default:
+                return 0;
+        }
+    }
+    public double fitness(Object [] chromosome) {
+        int [] numbers = {2,5,9,10};
+        int c = 0;
+        for(int i=0;i<numbers.length;i++) {
+            if(i == 0) {
+                c = operation(c,numbers[i],"+");
+            }else{
+                c = operation(c,numbers[i],(String) chromosome[i-1]);
+            }
+        }
+        int d = Math.abs(53-c);
+        return (double) 1/(1+d);
+    }
+
+    /* -------------------------------------------- */
+    /* -------------------------------------------- */
+
+    public static void main(String [] args) {
+        Object [] alphabet = {"+","-","*"}; // Alphabet
+        Config c = new Config(
+                alphabet,0.8,0,15,100,3,0,"/Users/enolalvarezdeprado/Dropbox/Mis cosas/GeneticAI/oye.txt"
+        );
+        NumberSum numberSum = new NumberSum();
+        numberSum.setConfig(c);
+        numberSum.init();
+        numberSum.show();
+    }
+
+}
---
+++ examples/PresentsBuy.java
@@ -0,0 +1,101 @@
+package examples;
+
+import source.Algorithm;
+import source.Config;
+
+/*
+    PROBLEM: An AI investigator wants to buy 5 presents.
+    The information concercing price and rating on Amazon is given by the Present class. The 20 presents available are represented by
+    the "presents" vector of the PresentBuy class.
+    We want the sum of the price of the five presents to be as close as possible to 300 (the investigator's budget) and the difference
+    betweeen the highest and the lowest price to be as small as possible.
+
+    MODELING:
+    - Chromosome of 5 genes, each of them representing the number of the present chosen in that Individual.
+    - Fitness function: 1/(1+r+s)
+    where
+        r = abs(300 - sumOfPrices)
+        s = maxPrice - minPrice
+ */
+
+class Present{
+    private int price; // Price of the present
+    private int rating; // Rating of the product in Amazon
+
+    public Present(int price, int rating) {
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+    public int getRating() {
+        return this.rating;
+    }
+}
+
+public class PresentsBuy extends Algorithm {
+    private Present [] presents = new Present[20];
+
+    public PresentsBuy() {
+        this.setPresents();
+    }
+
+    public void setPresents() {
+        int [] prices = {
+                62,55,17,115,40,65,80,83,99,49,25,30,22,30,30,50,70,60,58,110
+        };
+        int [] ratings = {
+                1,4,3,2,5,1,1,2,5,4,3,4,4,3,2,3,5,4,4,5
+        };
+        for(int i=0;i<presents.length;i++)
+            this.presents[i] = new Present(prices[i],ratings[i]);
+    }
+
+    /* -------------- FITNESS FUNCTION -------------- */
+    public int getMax(Object [] chromosome) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<chromosome.length;i++)
+            if( this.presents[((Integer) chromosome[i]).intValue()].getPrice() >  max) max = this.presents[((Integer) chromosome[i]).intValue()].getPrice();
+        return max;
+    }
+
+    public int getMin(Object [] chromosome) {
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<chromosome.length;i++)
+            if( this.presents[((Integer) chromosome[i]).intValue()].getPrice() < min ) min = this.presents[((Integer) chromosome[i]).intValue()].getPrice();
+        return min;
+    }
+
+    public double fitness(Object [] chromosome) {
+        int totalCost = 0;
+        double fitness = 0.0;
+        int r, s;
+        for(int i=0;i<chromosome.length;i++)
+            totalCost += this.presents[((Integer) chromosome[i]).intValue()].getPrice();
+        r = Math.abs(300-totalCost);
+        s = this.getMax(chromosome) - this.getMin(chromosome);
+        if(totalCost <= 300) {
+            fitness = (double) 1/(1+r+s);
+        }
+        return fitness;
+    }
+
+    /* -------------------------------------------- */
+    /* -------------------------------------------- */
+
+    public static void main(String [] args) {
+        Object [] alphabet = new Integer[20]; // Alphabet
+        for(int i=0;i<20;i++) alphabet[i] = new Integer(i); // Generate alphabet; Representing the number of the present in the "presents" vector
+        Config c = new Config(
+                alphabet,0.6,0,20,400,5,10
+        );
+        PresentsBuy presentsBuy = new PresentsBuy();
+        presentsBuy.setConfig(c);
+        presentsBuy.init();
+        presentsBuy.show();
+        // presentsBuy.toFile(); /* You must provide a valid file */
+    }
+
+}
